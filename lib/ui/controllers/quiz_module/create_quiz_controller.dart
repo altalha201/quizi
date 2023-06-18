36d77b1a32ffedf526da3c9a41df76345ec588cb
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-06-18 20:05:15 +0600

    update

--- lib/ui/controllers/quiz_module/create_quiz_controller.dart
+++ lib/ui/controllers/quiz_module/create_quiz_controller.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
+import 'package:quizi/data/models/quiz_model/normal_quiz_model.dart';
+
+import '../../../data/models/quiz_model/question.dart';
 
 class CreateQuizController extends GetxController {
   bool _creatingQuiz = false;
 
   bool get creatingQuiz => _creatingQuiz;
 
-  Future<void> createNormalQuiz() async {
+  Future<void> createNormalQuiz(NormalQuizModel quizModel, List<Question> questions) async {
     _creatingQuiz = true;
     update();
   }
